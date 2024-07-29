d270be42d36d63af6b8a84c27be2c93f018493a8
Sabrina Yafa <yafasabrina2323@gmail.com>
2024-07-29 10:45:24 +0100

    nvm bro

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -93,7 +93,7 @@ const ProductDetails = async({params :{ id } }:Props) => {
               />
             </div>
           </div>
-          <Modal />
+          <Modal productId={id}/>
         </div>
       </div>
       <div className='flex flex-col gap-12 '>
